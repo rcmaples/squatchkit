b12915419f98b3eacc1c1cb79c25d4474cd36ac6
RC Maples <rc@fullstory.com>
2022-12-27 15:31:13 -0500

    clearing out demo stuff

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,17 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    '@storybook/preset-create-react-app',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/preset-scss',
+    '@storybook/preset-typescript',
   ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+  },
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
