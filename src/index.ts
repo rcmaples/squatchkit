a88521fc7fc1526e08aa54a80781f96be293ad57
RC Maples <rc@rcmaples.io>
2023-01-01 18:51:59 -0400

    cleanedup index.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// import Button from './components/Button/Button'
-
 export { default as Button } from './components/Button/Button'
